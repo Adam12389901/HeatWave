499c6c758094cd36721f1af71da0d9149b5b1d1a
justeating <128ABodrov@frhsd.com>
2026-09-14 10:31:16 -0400

    working on cleanData wrote all the methods

--- src/WeatherData.java
+++ src/WeatherData.java
@@ -1,8 +1,24 @@
 public class WeatherData {
+
+    private ArrayList<Double> temperatures;
+
+
+    public void cleanData(double lower, double upper) {
+        for(int i = 0; i < temperatures.size(); i++){
+            if (temperatures.get(i) < 85.0 && temperatures.get(i) > 120.0){
+                temperatures.remove(i);
+                i--;
+            }
+        }
+    }
+
+    public int longestHeatWave(double threshold) {
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-
 
 
 
