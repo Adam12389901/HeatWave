ae8aa72b11dab5a8f6e6695804d84bac80690f7e
Adam12389901 <128Akonikov@frhsd.com>
2026-09-14 10:54:21 -0400

    Made WeatherData constructor for the arraylist variables

--- src/WeatherData.java
+++ src/WeatherData.java
@@ -5,6 +5,10 @@ public class WeatherData {
     private static ArrayList<Double> temperatures;
 
 
+    public WeatherData() {
+        temperatures = new ArrayList<Double>();
+    }
+
     public void cleanData(double lower, double upper) {
         for(int i = 0; i < temperatures.size(); i++){
             if (temperatures.get(i) < 85.0 && temperatures.get(i) > 120.0){
@@ -30,12 +34,15 @@ public class WeatherData {
     }
 
     public static void main(String[] args) {
-        temperatures.add(100.7);
-        temperatures.add(120.6);
-        temperatures.add(120.7);
-        temperatures.add(120.6);
-        temperatures.add(120.7);
-        System.out.println(temperatures.get(0));
+        temperatures.add(99.1);
+        temperatures.add(85.0);
+        temperatures.add(85.1);
+        temperatures.add(94.3);
+        temperatures.add(98.0);
+        temperatures.add(101.0);
+        temperatures.add(102.5);
+        WeatherData weatherData = new WeatherData();
+
     }
 
 
